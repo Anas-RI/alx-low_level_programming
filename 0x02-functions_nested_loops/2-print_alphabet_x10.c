abde4b004c66680346a10bb3d35f267cc09bc52c
Anas <anas.elammari49@gmail.com>
2023-03-21 16:37:07 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+  * print_alphabet_x10 - yes, same stuff just 10 times more
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10_optimized.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+  * print_alphabet_x10 - yes, same stuff just 10 times more
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	for ( int i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
