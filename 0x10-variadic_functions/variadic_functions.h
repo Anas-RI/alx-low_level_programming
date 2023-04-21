cc10df3616a936735e9ca8faeceaa0c211b37784
Anas <anas.elammari49@gmail.com>
2023-04-20 19:32:12 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,4 +10,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
 
+void print_all(const char * const format, ...);
+
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
 #endif /* VARIADIC_FUNCTIONS_H */
