e485d434f5346a3a69dd40e9a2fbf2004b2db0b1
Anas <anas.elammari49@gmail.com>
2023-04-27 11:55:57 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * first - prints a sentence before the main func
+ */
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
